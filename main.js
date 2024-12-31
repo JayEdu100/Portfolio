4d46801b6c5ccb465ea3e41e1260b4630f17f070
JayEdu100 <jay.education100@gmail.com>
2024-12-31 14:03:20 +1300

    data structure

---
+++ main.js
@@ -0,0 +1,44 @@
+const pronouns = [
+    "he",
+    "she",
+    "it",
+    "they",
+    "we",
+    "you",
+    "I",
+    "me",
+    "us",
+    "them"
+];
+
+const verbs = [
+    "eat",
+    "pick",
+    "run",
+    "jump",
+    "write",
+    "read",
+    "sing",
+    "dance",
+    "swim",
+    "play"
+];
+
+const fruitsAndVeggies = [
+    "apple",
+    "banana",
+    "carrot",
+    "broccoli",
+    "grape",
+    "orange",
+    "spinach",
+    "strawberry",
+    "tomato",
+    "cucumber"
+];
+
+const bundledObject = {
+    pronouns: pronouns,
+    verbs: verbs,
+    fruitsAndVeggies: fruitsAndVeggies
+};
